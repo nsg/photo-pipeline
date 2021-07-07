9d033ef5d56b80f2d9c04a0f105602ad8a48ca86
Stefan Berggren <nsg@nsg.cc>
2021-07-07 13:55:16 +0200

    Rename consumer to photo-channel

--- splitter.py
+++ splitter.py
@@ -4,5 +4,5 @@ import sys
 from subprocess import Popen, PIPE, STDOUT
 
 for line in sys.stdin:
-    p = Popen(['./consumer.sh', 'process'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    p = Popen(['./photo-channel.sh', 'process'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
     print(p.communicate(input=line.encode())[0].decode(), end="")
