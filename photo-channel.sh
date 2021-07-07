0303c8278b40989155b5326120f34c0499fbb982
Stefan Berggren <nsg@nsg.cc>
2021-07-07 14:19:04 +0200

    Send the payload down to the output plugins

--- photo-channel.sh
+++ photo-channel.sh
@@ -2,12 +2,26 @@
 
 . config.conf
 
+output_pipeline() {
+    local total=${#OUTPUT_PLUGINS[@]}
+    local current="$1"
+
+    if [ $current -ge $total ]; then
+        cat -
+    else
+        local plugin="${OUTPUT_PLUGINS[$current]}"
+        local image=${plugin%#*}
+        local params=${plugin#*#}
+        cat - | sudo docker run -iv $params:/out $image | output_pipeline $(( $current + 1 ))
+    fi
+}
+
 process_pipeline() {
     local total=${#PROCESS_PLUGINS[@]}
     local current="$1"
 
     if [ $current -ge $total ]; then
-        cat -
+        cat - | output_pipeline 0
     else
         local image="${PROCESS_PLUGINS[$current]}"
         cat - | sudo docker run -i $image | process_pipeline $(( $current + 1 ))
